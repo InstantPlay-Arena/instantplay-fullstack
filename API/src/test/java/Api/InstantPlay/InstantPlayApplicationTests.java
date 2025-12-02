20f6e7934d249c7eedad85fc1ca7516b099611fe
Darkmatmat <mateo.leonard25@gmail.com>
2025-12-02 15:10:24 +0100

    🛠️ build: initialisation API REST PROJECT

---
+++ API/src/test/java/Api/InstantPlay/InstantPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package Api.InstantPlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
